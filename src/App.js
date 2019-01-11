057f18b59e0df3e8a0fb9d40a6c612a76cf1e717
Nirma Perera <nirmaperera23@gmail.com>
2019-01-11 09:39:19 -0500

    foler toggle react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+
+class Folder extends Component{
+   constructor(props) {
+    super(props);
+    this.state = {isToggle: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({isToggle: !this.state.isToggle});
+  }
+
+  render() { 
+    return (
+      <div className="myFiles">
+        <button
+          className="btn"
+          onClick={this.handleClick}
+        >
+          Toggle
+        </button>
+        <h1 className="folerTitle">Home</h1>
+        
+        <div
+        // display(block) or hide (none)
+          style={{display: this.state.isToggle ? 'block': 'none'}}
+          className="container"
+        >
+          <li>File1</li>
+          <li>File2</li>
+          <li>File3</li>
+         
+        </div>
+      </div>
+      );
+   }
+}
+
+
+//main component
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+           <Folder/>
       </div>
     );
   }
 }
+
+
 
 export default App;
